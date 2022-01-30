330dca94dcf0c546de92be3bfdc30fbe5da3cad2
Vinnie Marone <vinnie.marone35@gmail.com>
2022-01-30 09:52:26 -0500

    Add an exception code to VultrException

--- src/VultrException.php
+++ src/VultrException.php
@@ -6,5 +6,10 @@ use Exception;
 
 class VultrException extends Exception
 {
+	public const DEFAULT_CODE = 300;
 
+	public function __construct(string $message, int $code = self::DEFAULT_CODE)
+	{
+		parent::__construct($message, $code);
+	}
 }
---
+++ tests/VultrExceptionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Vultr\VultrPhp\Tests;
+
+use Vultr\VultrPhp\VultrException;
+use PHPUnit\Framework\TestCase;
+
+class VultrExceptionTest extends TestCase
+{
+	public function testDefaultException()
+	{
+		try
+		{
+			throw new VultrException('Test Default');
+		}
+		catch (VultrException $e)
+		{
+			$this->assertEquals(VultrException::DEFAULT_CODE, $e->getCode());
+		}
+	}
+}
