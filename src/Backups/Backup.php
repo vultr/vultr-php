bcdd8a8a2d63188580d1b25144bf904e354e1922
Porthorian <vinnie.marone35@gmail.com>
2022-02-05 10:39:05 -0500

    Create Backup Model

--- src/Backups/Backup.php
+++ src/Backups/Backup.php
@@ -6,5 +6,49 @@ use Vultr\VultrPhp\Util\Model;
 
 class Backup extends Model
 {
+	protected int $id;
+	protected string $dateCreated;
+	protected string $description;
+	protected int $size;
+	protected string $status;
 
+	public function getId() : int
+	{
+		return $this->id;
+	}
+
+	public function setId(int $id) : void
+	{
+		$this->id = $id;
+	}
+
+	public function getDateCreated() : string
+	{
+		return $this->dateCreated;
+	}
+
+	public function setDateCreated(string $date_created) : void
+	{
+		$this->dateCreated = $date_created;
+	}
+
+	public function getDescription() : string
+	{
+		return $this->description;
+	}
+
+	public function setDescription(string $description) : void
+	{
+		$this->description = $description;
+	}
+
+	public function getSize() : int
+	{
+		return $this->size;
+	}
+
+	public function setSize(int $size) : void
+	{
+		$this->size = $size;
+	}
 }
