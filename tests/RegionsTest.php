bc19549d32beffc8bae8878d0767a878c60269df
Porthorian <vinnie.marone35@gmail.com>
2022-02-07 23:19:53 -0500

    Add Plans, Regions, Snapshots test skeletons

---
+++ tests/RegionsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vultr\VultrPhp\Tests;
+
+use Vultr\VultrPhp\VultrClient;
+
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Exception\RequestException;
+
+use PHPUnit\Framework\TestCase;
+
+class RegionsTest extends TestCase
+{
+
+}
