5240a19a00b4347f66095547bbec0b004589b801
Porthorian <vinnie.marone35@gmail.com>
2022-02-07 23:00:48 -0500

    Add VultrServiceException to tests

--- tests/VultrExceptionTest.php
+++ tests/VultrExceptionTest.php
@@ -3,6 +3,8 @@
 namespace Vultr\VultrPhp\Tests;
 
 use Vultr\VultrPhp\VultrException;
+use Vultr\VultrPhp\Services\VultrServiceException;
+
 use PHPUnit\Framework\TestCase;
 
 class VultrExceptionTest extends TestCase
@@ -16,7 +18,7 @@ class VultrExceptionTest extends TestCase
 		catch (VultrException $e)
 		{
 			$this->assertEquals(VultrException::DEFAULT_CODE, $e->getCode());
-			$this->assertEquals(null, $e->getHTTPCode());
+			$this->assertNull($e->getHTTPCode());
 		}
 
 		$test_code = 301;
@@ -26,6 +28,31 @@ class VultrExceptionTest extends TestCase
 			throw new VultrException('Test Default 2', $test_code, $test_http, null);
 		}
 		catch (VultrException $e)
+		{
+			$this->assertEquals($test_code, $e->getCode());
+			$this->assertEquals($test_http, $e->getHTTPCode());
+		}
+	}
+
+	public function testVultrServiceException()
+	{
+		try
+		{
+			throw new VultrServiceException('Test');
+		}
+		catch (VultrServiceException $e)
+		{
+			$this->assertEquals(VultrException::SERVICE_CODE, $e->getCode());
+			$this->assertNull($e->getHTTPCode());
+		}
+
+		$test_code = 302;
+		$test_http = 400;
+		try
+		{
+			throw new VultrServiceException('Test', $test_code, $test_http);
+		}
+		catch (VultrServiceException $e)
 		{
 			$this->assertEquals($test_code, $e->getCode());
 			$this->assertEquals($test_http, $e->getHTTPCode());
