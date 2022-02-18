8993ec20353947cfe274a4a48f6b093c0ed85e01
Porthorian <vinnie.marone35@gmail.com>
2022-02-18 17:37:27 -0500

    Add tests for VultrUtil Functions

--- src/Util/VultrUtil.php
+++ src/Util/VultrUtil.php
@@ -20,24 +20,45 @@ class VultrUtil
 	 * @param $prop - The arching prop that has the contents that we will map the object to.
 	 * Ex a Backup object will have "backup" : { 'id', etc etc}
 	 * So in $prop we would specifiy "backup"
+	 * @throws VultrException
 	 * @return ModelInterface
 	 */
 	public static function mapObject(stdClass $stdclass, ModelInterface $model, ?string $prop = null) : ModelInterface
 	{
-		$map = (new JsonMapperFactory())->bestFit();
-		$map->push(new CaseConversion(TextNotation::UNDERSCORE(), TextNotation::CAMEL_CASE()));
-
 		$object = $stdclass;
 		if ($prop !== null)
 		{
 			$object = $stdclass->$prop;
 		}
 
-		$model->resetObject();
+		$mapped_object = $model;
+		try
+		{
+			$map = (new JsonMapperFactory())->bestFit();
+			$map->push(new CaseConversion(TextNotation::UNDERSCORE(), TextNotation::CAMEL_CASE()));
 
-		return $map->mapObject($object, clone $model);
+			$model->resetObject();
+
+			$mapped_object = $map->mapObject($object, clone $model);
+		}
+		catch (Throwable $e)
+		{
+			throw new VultrException('Failed to map object for '.get_class($model).': '.$e->getMessage(), VultrException::DEFAULT_CODE, null, $e);
+		}
+
+		return $mapped_object;
 	}
 
+	/**
+	 * @param $json - string - Raw JSON
+	 * @param $model - A model that will be used to map the json response to the object.
+	 * Model is passed in via reference, the model will immediately be reset before its mapped and cloned.
+	 * @param $prop - The arching prop that has the contents that we will map the object to.
+	 * Ex a Backup object will have "backup" : { 'id', etc etc}
+	 * So in $prop we would specifiy "backup"
+	 * @throws VultrException
+	 * @return ModelInterface
+	 */
 	public static function convertJSONToObject(string $json, ModelInterface $model, ?string $prop = null) : ModelInterface
 	{
 		$class_name = get_class($model);
---
+++ tests/Suite/VultrUtilTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Vultr\VultrPhp\Tests\Suite;
+
+use stdClass;
+use Vultr\VultrPhp\VultrException;
+use Vultr\VultrPhp\Util\VultrUtil;
+use Vultr\VultrPhp\Tests\VultrTest;
+use Vultr\VultrPhp\Tests\Data\VultrUtilData;
+
+class VultrUtilTest extends VultrTest
+{
+	public function testMapObject()
+	{
+		$std_class = new stdClass();
+		$std_class->id = 1;
+		$std_class->value1 = 'addddsdsd';
+		$std_class->value2 = 69;
+		$std_class->value3 = 4.20;
+		$std_class->value4 = ['spaghetti', 'more spaghetti'];
+		$std_class->value5 = false;
+
+		$object = VultrUtil::mapObject($std_class, new VultrUtilData());
+
+		foreach ($object->toArray() as $attr => $value)
+		{
+			$this->assertEquals($value, $std_class->$attr);
+		}
+	}
+
+	public function testConvertJSONToObject()
+	{
+		$array = [
+			'id' => 1,
+			'value1' => 'addasdasdfasf',
+			'value2' => 69,
+			'value3' => 4.20,
+			'value4' => ['spaghetti', 'bolognese'],
+			'value5' => false
+		];
+		$json = json_encode($array);
+
+		$object = VultrUtil::convertJSONToObject(json_encode($array), new VultrUtilData());
+		foreach ($object->toArray() as $attr => $value)
+		{
+			$this->assertEquals($value, $array[$attr]);
+		}
+
+		$test = [];
+		$test['test'] = $array;
+		$object = VultrUtil::convertJSONToObject(json_encode($test), new VultrUtilData(), 'test');
+		foreach ($object->toArray() as $attr => $value)
+		{
+			$this->assertEquals($value, $test['test'][$attr]);
+		}
+
+		$this->expectException(VultrException::class);
+		$json = 'asdasfasfs';
+		VultrUtil::convertJSONToObject($json, new VultrUtilData());
+	}
+}
