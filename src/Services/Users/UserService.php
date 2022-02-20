f08f661614aad0ff8bdb1547afca168d36b60eb4
Porthorian <vinnie.marone35@gmail.com>
2022-02-20 02:48:16 -0500

    Add more of a User skeleton

--- src/Services/Users/UserService.php
+++ src/Services/Users/UserService.php
@@ -8,6 +8,29 @@ use Vultr\VultrPhp\Util\ListOptions;
 
 class UserService extends VultrService
 {
+	public function getUser() : User
+	{
 
+	}
+
+	public function getUsers(?ListOptions &$options = null) : array
+	{
+
+	}
+
+	public function createUser(User $user) : User
+	{
+
+	}
+
+	public function updateUser(User $user) : void
+	{
+
+	}
+
+	public function deleteUser(string $user_id) : void
+	{
+
+	}
 }
 
