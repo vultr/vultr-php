aaf4d29e000d74c838a43cbe38971eb7868ef3ca
Porthorian <vinnie.marone35@gmail.com>
2022-02-20 01:36:44 -0500

    Add user skeleton

---
+++ src/Services/Users/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Vultr\VultrPhp\Services\Users;
+
+use Vultr\VultrPhp\Util\Model;
+
+class User extends Model
+{
+
+}
---
+++ src/Services/Users/UserService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Vultr\VultrPhp\Services\Users;
+
+use Vultr\VultrPhp\Services\VultrServiceException;
+use Vultr\VultrPhp\Services\VultrService;
+use Vultr\VultrPhp\Util\ListOptions;
+
+class UserService extends VultrService
+{
+
+}
+
