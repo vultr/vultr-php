88d498b6ee5b6bf58eedcd62572270344cf0eb19
Porthorian <vinnie.marone35@gmail.com>
2022-03-04 21:20:40 -0500

    Add SSHKeysTest skeleton

---
+++ tests/Suite/SSHKeysTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Vultr\VultrPhp\Tests\Suite;
+
+use Vultr\VultrPhp\VultrClient;
+
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
+
+use Vultr\VultrPhp\Tests\VultrTest;
+
+class SSHKeysTest extends VultrTest
+{
+	public function testGetSSHKey()
+	{
+		$this->markTestSkipped('Incomplete');
+	}
+
+	public function testGetSSHKeys()
+	{
+		$this->markTestSkipped('Incomplete');
+	}
+
+	public function testUpdateSSHKey()
+	{
+		$this->markTestSkipped('Incomplete');
+	}
+
+	public function testDeleteSSHKey()
+	{
+		$this->markTestSkipped('Incomplete');
+	}
+
+	public function testCreateSSHKey()
+	{
+		$this->markTestSkipped('Incomplete');
+	}
+}
