ea4a2db4475c64dad5add46a881e8389c071b166
Porthorian <vinnie.marone35@gmail.com>
2022-03-04 20:04:51 -0500

    Add testGetStartupScript test

---
+++ tests/Data/StartupScriptsData.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Vultr\VultrPhp\Tests\Data;
+
+use Vultr\VultrPhp\Tests\DataProvider;
+
+class StartupScriptsData extends DataProvider
+{
+	protected function dataGetStartupScript(string $startup_id) : array
+	{
+		return json_decode('{
+  "startup_script": {
+    "id": "'.$startup_id.'",
+    "date_created": "2020-10-10T01:56:20+00:00",
+    "date_modified": "2020-10-10T01:59:20+00:00",
+    "name": "Example Startup Script",
+    "type": "pxe",
+    "script": "QmFzZTY0IEV4YW1wbGUgRGF0YQ=="
+  }
+}', true);
+	}
+}
--- tests/Suite/StartupScriptsTest.php
+++ tests/Suite/StartupScriptsTest.php
@@ -3,6 +3,7 @@
 namespace Vultr\VultrPhp\Tests\Suite;
 
 use Vultr\VultrPhp\VultrClient;
+use Vultr\VultrPhp\Services\StartupScripts\StartupScriptException;
 
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\Request;
@@ -14,7 +15,22 @@ class StartupScriptsTest extends VultrTest
 {
 	public function testGetStartupScript()
 	{
-		$this->markTestSkipped('Incomplete');
+		$startup_id = 'cb676a46-66fd-4dfb-b839-443f2e6c0b60';
+		$data = $this->getDataProvider()->getData($startup_id);
+
+		$client = $this->getDataProvider()->createClientHandler([
+			new Response(200, ['Content-Type' => 'application/json'], json_encode($data)),
+			new RequestException('Bad Request', new Request('GET', 'startup-scripts/'.$startup_id), new Response(400, [], json_encode(['error' => 'Bad Request']))),
+		]);
+
+		$script = $client->startup_scripts->getStartupScript($startup_id);
+		foreach ($script->toArray() as $attr => $value)
+		{
+			$this->assertEquals($value, $data['startup_script'][$attr]);
+		}
+
+		$this->expectException(StartupScriptException::class);
+		$client->startup_scripts->getStartupScript($startup_id);
 	}
 
 	public function testGetStartupScripts()
