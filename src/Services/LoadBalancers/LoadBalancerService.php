c7b6eecf2703439c6bb2310ac152b8e2c1a1367c
Porthorian <vinnie.marone35@gmail.com>
2022-06-19 22:05:12 -0400

    Implement getLoadBalancers()

--- src/Services/LoadBalancers/LoadBalancerService.php
+++ src/Services/LoadBalancers/LoadBalancerService.php
@@ -13,11 +13,37 @@ class LoadBalancerService extends VultrService
 	 * @see https://www.vultr.com/api/#operation/get-load-balancer
 	 * @param $id - string - Example: cb676a46-66fd-4dfb-b839-443f2e6c0b60
 	 * @throws LoadBalancerException
+	 * @throws VultrException
 	 * @return LoadBalancer
 	 */
 	public function getLoadBalancer(string $id) : LoadBalancer
 	{
 		$object = $this->getObject('load-balancers/'.$id, new LoadBalancer());
+		$this->setRules($object);
+		return $object;
+	}
+
+	/**
+	 * @see https://www.vultr.com/api/#operation/list-load-balancers
+	 * @param $options - ListOptions|null - Interact via reference.
+	 * @throws LoadBalancerException
+	 * @return LoadBalancer[]
+	 */
+	public function getLoadBalancers(?ListOptions &$options = null) : array
+	{
+		$objects = $this->getListObjects('load-balancers', new LoadBalancer(), $options);
+		foreach ($objects as &$object)
+		{
+			$this->setRules($object);
+		}
+		return $objects;
+	}
+
+	/**
+	 * @throws VultrException
+	 */
+	private function setRules(LoadBalancer &$object) : void
+	{
 		$rules = [];
 		foreach ($object->getForwardingRules() as $rule)
 		{
@@ -31,17 +57,5 @@ class LoadBalancerService extends VultrService
 			$rules[] = VultrUtil::mapObject($rule, new FirewallRule());
 		}
 		$object->setFirewallRules($rules);
-
-		return $object;
-	}
-
-	/**
-	 * @see https://www.vultr.com/api/#operation/list-load-balancers
-	 * @throws LoadBalancerException
-	 * @return LoadBalancer[]
-	 */
-	public function getLoadBalancers() : array
-	{
-
 	}
 }
--- tests/Suite/LoadBalancersTest.php
+++ tests/Suite/LoadBalancersTest.php
@@ -24,6 +24,42 @@ class LoadBalancersTest extends VultrTest
 
 		$id = 'cb676a46-66fd-4dfb-b839-443f2e6c0b60';
 		$response_object = $client->loadbalancers->getLoadBalancer($id);
+		$this->testObject($response_object, $spec_data[$response_object->getResponseName()]);
+
+		$this->expectException(LoadBalancerException::class);
+		$client->loadbalancers->getLoadBalancer($id);
+	}
+
+	public function testGetLoadBalancers()
+	{
+		$provider = $this->getDataProvider();
+		$spec_data = $provider->getData();
+
+		$client = $provider->createClientHandler([
+			new Response(200, ['Content-Type' => 'application/json'], json_encode($spec_data)),
+			new Response(400, [], json_encode(['error' => 'Bad request'])),
+		]);
+
+		foreach ($client->loadbalancers->getLoadBalancers() as $response_object)
+		{
+			$data = null;
+			foreach ($spec_data[$response_object->getResponseListName()] as $json_object)
+			{
+				if ($json_object['id'] !== $response_object->getId()) continue;
+
+				$data = $json_object;
+				break;
+			}
+			$this->assertNotNull($data);
+			$this->testObject($response_object, $data);
+		}
+
+		$this->expectException(LoadBalancerException::class);
+		$client->loadbalancers->getLoadBalancers();
+	}
+
+	private function testObject(ModelInterface $response_object, array $spec_data)
+	{
 		$this->assertInstanceOf(LoadBalancer::class, $response_object);
 		$array = $response_object->toArray();
 		foreach ($array as $attr => $value)
@@ -39,24 +75,21 @@ class LoadBalancersTest extends VultrTest
 					$potential = $potential->toArray();
 				}
 			}
-			else if (is_array($object_data))
+			else if (is_array($object_data) && in_array($attr, ['forwarding_rules', 'firewall_rules']))
 			{
 				foreach ($object_data as &$potential)
 				{
-					if (!($potential instanceof ModelInterface)) continue;
+					$this->assertInstanceOf(ModelInterface::class, $potential);
 
 					$potential = $potential->toArray();
 				}
 			}
-			$this->assertEquals($object_data, $spec_data[$response_object->getResponseName()][$attr], "Attribute {$attr} failed to meet comparison against spec.");
+			$this->assertEquals($object_data, $spec_data[$attr], "Attribute {$attr} failed to meet comparison against spec.");
 		}
 
-		foreach (array_keys($spec_data[$response_object->getResponseName()]) as $attr)
+		foreach (array_keys($spec_data) as $attr)
 		{
 			$this->assertTrue(array_key_exists($attr, $array), "Attribute {$attr} failed to exist in toArray of the response object.");
 		}
-
-		$this->expectException(LoadBalancerException::class);
-		$client->loadbalancers->getLoadBalancer($id);
 	}
 }
